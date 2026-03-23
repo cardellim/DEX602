061a4d57309610236cf9c5da439b4407907c0a39
Mark Cardelli <mark.cardelli@underarmour.com>
2026-03-23 15:52:21 +0000

    Exercise 2-5 Completed

--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,12 +1,16 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api } from "lwc";
 
 export default class StudentTile extends LightningElement {
-    @api student = {
-        Name: 'Mark Cardelli',
-        PhotoUrl: '/services/images/photo/003B0FakePictId',
-    };
-    @api isSelected = false;
-    get tileSelected() {
-        return this.isSelected ? "tile selected" : "tile";
-    }
+	@api student = {
+		Name: "Mark Cardelli",
+		PhotoUrl: "/services/images/photo/003B0FakePictId"
+	};
+	@api isSelected = false;
+	get tileSelected() {
+		return this.isSelected ? "tile selected" : "tile";
+	}
+
+	handleStudentClick() {
+		alert(this.student.Name);
+	}
 }
