b527d400efe13110e718c45148374a430af352b3
Mark Cardelli <mark.cardelli@underarmour.com>
2026-03-24 15:11:57 +0000

    Exercise 2-9 Completed

--- force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
+++ force-app/Exercises/main/default/lwc/studentBrowserForm/studentBrowserForm.js
@@ -1,13 +1,16 @@
 import { LightningElement, wire } from "lwc";
+import { NavigationMixin } from "lightning/navigation";
+import { encodeDefaultFieldValues } from "lightning/pageReferenceUtils";
 import getInstructors from "@salesforce/apex/StudentBrowserForm.getInstructors";
 import getDeliveriesByInstructor from "@salesforce/apex/StudentBrowserForm.getDeliveriesByInstructor";
 
-export default class StudentBrowserForm extends LightningElement {
+export default class StudentBrowserForm extends NavigationMixin(LightningElement) {
 	instructors = [];
 	error;
 	selectedInstructorId = "";
 	deliveries = [];
 	selectedDeliveryId = "";
+	isButtonDisabled = true;
 
 	@wire(getInstructors)
 	wired_getInstructors({ error, data }) {
@@ -46,12 +49,31 @@ export default class StudentBrowserForm extends LightningElement {
 	handleInstructorChange(event) {
 		this.selectedDeliveryId = "";
 		this.selectedInstructorId = event.target.value;
+		this.isButtonDisabled = this.selectedInstructorId === "";
 		this.notifyParent();
 	}
 
 	handleDeliveryChange(event) {
 		this.selectedDeliveryId = event.target.value;
 		this.notifyParent();
+	}
+
+	handleAddNewDelivery() {
+		// Opens the new Course Delivery record modal dialog
+		// with the selected InstructorId prepopulated
+		const pageInfo = {
+			type: "standard__objectPage",
+			attributes: {
+				objectApiName: "Course_Delivery__c",
+				actionName: "new"
+			},
+			state: {
+				defaultFieldValues: encodeDefaultFieldValues({
+					Instructor__c: this.selectedInstructorId
+				})
+			}
+		};
+		this[NavigationMixin.Navigate](pageInfo);
 	}
 
 	notifyParent() {
