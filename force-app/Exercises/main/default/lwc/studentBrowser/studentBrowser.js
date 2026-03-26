f83121eef86f6f42d02804eb3d7790f55176f4c5
Mark Cardelli <mark.cardelli@underarmour.com>
2026-03-26 09:11:29 +0000

    Exercise 5-1 Completed

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,9 +1,10 @@
 import { LightningElement, wire } from "lwc";
 import { publish, MessageContext } from "lightning/messageService";
+import { NavigationMixin } from "lightning/navigation";
 import getStudents from "@salesforce/apex/StudentBrowser.getStudents";
 import SELECTED_STUDENT_CHANNEL from "@salesforce/messageChannel/SelectedStudentChannel__c";
 
-export default class StudentBrowser extends LightningElement {
+export default class StudentBrowser extends NavigationMixin(LightningElement) {
 	selectedInstructorId = "";
 	selectedDeliveryId = "";
 	@wire(getStudents, { instructorId: "$selectedInstructorId", courseDeliveryId: "$selectedDeliveryId" }) students;
@@ -43,6 +44,18 @@ export default class StudentBrowser extends LightningElement {
 	updateSelectedStudent(studentId) {
 		publish(this.messageContext, SELECTED_STUDENT_CHANNEL, {
 			studentId: studentId
+		});
+	}
+
+	handleRowDblClick(event) {
+		const studentId = event.detail.pk;
+		this[NavigationMixin.Navigate]({
+			type: "standard__recordPage",
+			attributes: {
+				recordId: studentId,
+				objectApiName: "Contact",
+				actionName: "edit"
+			}
 		});
 	}
 }
