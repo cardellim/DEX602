1b33ec60f5a50cf511bfdc9bf21f92b541d67c0b
Mark Cardelli <mark.cardelli@underarmour.com>
2026-03-26 17:12:14 +0000

    Exercise 6-1 Completed

---
+++ force-app/Exercises/main/default/lwc/tripReportForm/tripReportForm.js
@@ -0,0 +1,8 @@
+import { LightningElement } from "lwc";
+import Utils from "c/utils";
+
+export default class TripReportForm extends LightningElement {
+	handleSuccess() {
+		Utils.showToast(this, "Transaction Complete", "Your Trip Report was Saved");
+	}
+}
