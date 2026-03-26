91c722b0a580ff50ea17466a762382c40e8178dd
Mark Cardelli <mark.cardelli@underarmour.com>
2026-03-26 09:14:18 +0000

    Exercise 5-2 Updates StudentDetail

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -58,4 +58,9 @@ export default class StudentBrowser extends NavigationMixin(LightningElement) {
 			}
 		});
 	}
+
+	handleRowClick(event) {
+		const studentId = event.detail.pk;
+		this.updateSelectedStudent(studentId);
+	}
 }
