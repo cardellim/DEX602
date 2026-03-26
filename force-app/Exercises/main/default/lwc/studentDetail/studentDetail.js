2a787834a713074fe6da2ca3491f09724590c75e
Mark Cardelli <mark.cardelli@underarmour.com>
2026-03-26 09:33:05 +0000

    Exercise 5-3 Completed

--- force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
+++ force-app/Exercises/main/default/lwc/studentDetail/studentDetail.js
@@ -1,12 +1,13 @@
 import { LightningElement, wire } from "lwc";
 import { NavigationMixin } from "lightning/navigation";
 import { subscribe, unsubscribe, MessageContext } from "lightning/messageService";
-import { getRecord, getFieldValue, getFieldDisplayValue } from "lightning/uiRecordApi";
+import { getRecord } from "lightning/uiRecordApi";
 import FIELD_NAME from "@salesforce/schema/Contact.Name";
 import FIELD_DESCRIPTION from "@salesforce/schema/Contact.Description";
 import FIELD_EMAIL from "@salesforce/schema/Contact.Email";
 import FIELD_PHONE from "@salesforce/schema/Contact.Phone";
 import SELECTED_STUDENT_CHANNEL from "@salesforce/messageChannel/SelectedStudentChannel__c";
+import Utils from "c/utils";
 const fields = [FIELD_NAME, FIELD_DESCRIPTION, FIELD_EMAIL, FIELD_PHONE];
 
 export default class StudentDetail extends NavigationMixin(LightningElement) {
@@ -22,16 +23,16 @@ export default class StudentDetail extends NavigationMixin(LightningElement) {
 	//TODO #5: We provided a getter for the name field.
 	// 		   To prepare for Lab 1, create getters for the description, phone, and email fields.
 	get name() {
-		return this._getDisplayValue(this.wiredStudent.data, FIELD_NAME);
+		return Utils.getDisplayValue(this.wiredStudent.data, FIELD_NAME);
 	}
 	get description() {
-		return this._getDisplayValue(this.wiredStudent.data, FIELD_DESCRIPTION);
+		return Utils.getDisplayValue(this.wiredStudent.data, FIELD_DESCRIPTION);
 	}
 	get phone() {
-		return this._getDisplayValue(this.wiredStudent.data, FIELD_PHONE);
+		return Utils.getDisplayValue(this.wiredStudent.data, FIELD_PHONE);
 	}
 	get email() {
-		return this._getDisplayValue(this.wiredStudent.data, FIELD_EMAIL);
+		return Utils.getDisplayValue(this.wiredStudent.data, FIELD_EMAIL);
 	}
 	//TODO #6: Review the cardTitle getter, and the _getDisplayValue function below.
 	get cardTitle() {
@@ -42,10 +43,6 @@ export default class StudentDetail extends NavigationMixin(LightningElement) {
 			title = "Something went wrong...";
 		}
 		return title;
-	}
-
-	_getDisplayValue(data, field) {
-		return getFieldDisplayValue(data, field) ? getFieldDisplayValue(data, field) : getFieldValue(data, field);
 	}
 
 	connectedCallback() {
